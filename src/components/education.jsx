424b258d3cb3d66dfcf4cc3a2cfdb43985bea2c2
Harshal Shirote <harshalshirote2002@gmail.com>
2023-09-21 16:54:41 +0530

    Enhance Education Edit Features

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -1,30 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import myIcon from "../icons/edu.svg";
 import myIcon2 from "../icons/showMore.svg";
 import myIcon3 from "../icons/showLess.svg";
 
-export const eduData = [{
-  name: "Some College",
-  degree: "B.tech in Computer Science",
-  start: "2017-07-01",
-  end: "2021-07-05",
-  location: "MH, India",
-}];
+export const eduData = [
+  {
+    id: uuidv4(),
+    name: "Some College",
+    degree: "B.tech in Computer Science",
+    start: "2017-07-01",
+    end: "2021-07-05",
+    location: "MH, India",
+  },
+];
 
-function AddEducation({onCancel, onConfirm}) {
+function AddEducation({ onCancel, onConfirm }) {
+  const [formData, setFormData] = useState({
+    id: uuidv4(),
+    name: "",
+    degree: "",
+    start: "",
+    end: "",
+    location: "",
+  });
+  const handleConfirm = () => {
+    onConfirm(formData);
+  };
   return (
-    <>
+    <div>
       <div className="entry">
         <label htmlFor="education-name">School/College Name</label>
-        <input className="education-name" id="education-name" />
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, name: e.target.value });
+          }}
+          className="education-name"
+          id="education-name"
+        />
       </div>
       <div className="entry">
         <label htmlFor="education-degree">degree</label>
-        <input className="education-degree" id="education-degree" />
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, degree: e.target.value });
+          }}
+          className="education-degree"
+          id="education-degree"
+        />
       </div>
       <div className="entry">
         <label htmlFor="education-start-date">Start Date</label>
         <input
+          onChange={(e) => {
+            setFormData({ ...formData, start: e.target.value });
+          }}
           type="date"
           className="education-start-date"
           id="education-start-date"
@@ -33,6 +63,9 @@ function AddEducation({onCancel, onConfirm}) {
       <div className="entry">
         <label htmlFor="education-end-date">End Date</label>
         <input
+          onChange={(e) => {
+            setFormData({ ...formData, end: e.target.value });
+          }}
           type="date"
           className="education-end-date"
           id="education-end-date"
@@ -40,20 +73,115 @@ function AddEducation({onCancel, onConfirm}) {
       </div>
       <div className="entry">
         <label htmlFor="education-location">Location</label>
-        <input className="education-location" id="education-location" />
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, location: e.target.value });
+          }}
+          className="education-location"
+          id="education-location"
+        />
       </div>
-    </>
+      <div className="entry button-entry">
+        <button onClick={handleConfirm}>Confirm</button>
+        <button onClick={onCancel}>Cancel</button>
+      </div>
+    </div>
   );
 }
 
-function Section({ currentStateActive }) {
-  if (!currentStateActive) {
-    return <div className="education-inactive-section">
-      <h4>Education</h4>
-    </div>;
-  } else {
-    //
+function EditEdu({ onCancel, onConfirm, id }){
+  let initialExp = 0;
+  for (let i = 0; i < eduData.length; i++) {
+    if (eduData[i].id === id) {
+      initialExp = eduData[i];
+    }
   }
+  const [formData, setFormData] = useState(initialExp);
+  const handleConfirm = () => {
+    onConfirm(formData, formData.id);
+  };
+  return (
+    <div>
+      <div className="entry">
+        <label htmlFor="education-name">School/College Name</label>
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, name: e.target.value });
+          }}
+          value={formData.name}
+          className="education-name"
+          id="education-name"
+        />
+      </div>
+      <div className="entry">
+        <label htmlFor="education-degree">degree</label>
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, degree: e.target.value });
+          }}
+          value={formData.degree}
+          className="education-degree"
+          id="education-degree"
+        />
+      </div>
+      <div className="entry">
+        <label htmlFor="education-start-date">Start Date</label>
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, start: e.target.value });
+          }}
+          value={formData.start}
+          type="date"
+          className="education-start-date"
+          id="education-start-date"
+        />
+      </div>
+      <div className="entry">
+        <label htmlFor="education-end-date">End Date</label>
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, end: e.target.value });
+          }}
+          value={formData.end}
+          type="date"
+          className="education-end-date"
+          id="education-end-date"
+        />
+      </div>
+      <div className="entry">
+        <label htmlFor="education-location">Location</label>
+        <input
+          onChange={(e) => {
+            setFormData({ ...formData, location: e.target.value });
+          }}
+          value={formData.location}
+          className="education-location"
+          id="education-location"
+        />
+      </div>
+      <div className="entry button-entry">
+        <button onClick={handleConfirm}>Confirm</button>
+        <button onClick={onCancel}>Cancel</button>
+      </div>
+    </div>
+  );
+}
+
+function OldEdus({ data, setIndex }) {
+  return (
+    <>
+      {data.map((item) => (
+        <div
+          key={item.id}
+          id={item.id}
+          onClick={() => setIndex(item.id)}
+          className="education-inactive-section"
+        >
+          {item.name}
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default function Education({ onShow, isActive }) {
@@ -68,21 +196,72 @@ export default function Education({ onShow, isActive }) {
       </h3>
     );
   } else {
-    
-    return (
-      <div className="education-expanded">
-        <h3 onClick={onShow} className="education-head-expanded">
-          {" "}
-          <img src={myIcon} />
-          Education
-          <img src={myIcon3} />
-        </h3>
-        <Section currentStateActive={index === 0} />
-        <Section currentStateActive={index === 1} />
-        <div className="add-education">
-          <button>Add Education</button>
+    if (index === 0) {
+      return (
+        <div className="education-expanded">
+          <h3 onClick={onShow} className="education-head-expanded">
+            {" "}
+            <img src={myIcon} />
+            Education
+            <img src={myIcon3} />
+          </h3>
+          <OldEdus data={eduData} setIndex={setIndex} />
+          <div className="add-education">
+            <button
+              onClick={() => {
+                setIndex(1);
+              }}
+            >
+              Add Education
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else if (index === 1) {
+      const handleConfirm = (confirmedData) => {
+        eduData.push(confirmedData);
+        console.log(eduData);
+        setIndex(0);
+      };
+      return (
+        <div className="education-expanded">
+          <h3 onClick={onShow} className="education-head-expanded">
+            {" "}
+            <img src={myIcon} />
+            Education
+            <img src={myIcon3} />
+          </h3>
+          <AddEducation
+            onCancel={() => setIndex(0)}
+            onConfirm={handleConfirm}
+          />
+        </div>
+      );
+    }else{
+      const handleConfirm = (formData, id) => {
+        for (let i = 0; i < eduData.length; i++) {
+          if (eduData[i].id === id) {
+            eduData[i] = formData;
+          }
+        }
+        console.log(eduData);
+        setIndex(0);
+      };
+      return (
+        <div className="education-expanded">
+          <h3 onClick={onShow} className="education-head-expanded">
+            {" "}
+            <img src={myIcon} />
+            Education
+            <img src={myIcon3} />
+          </h3>
+          <EditEdu
+            onCancel={() => setIndex(0)}
+            onConfirm={handleConfirm}
+            id={index}
+          ></EditEdu>
+        </div>
+      );
+    }
   }
 }
